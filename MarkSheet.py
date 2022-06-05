de9a2957bf640b901e26d140800ef37cbd35a4b0
shahzad khan <shahzadkhanz@gmail.com>
2022-06-05 19:21:44 +0500

    First Assignment

---
+++ MarkSheet.py
@@ -0,0 +1,26 @@
+aa = "--------------------- Board of Intermediate ------------------------"
+print(aa)
+
+a = "Name: S Khan"
+print(a)
+b = "Father Name: M S Khan"
+print(b)
+c = "Roll No. 20112"
+print(c)
+d = "Accounting: 95"
+print(d)
+e = "Business Maths: 88"
+print(e)
+f = "Business Communication: 77"
+print(f)
+g = "Pakistan Studies: 79"
+print(g)
+h = "Islamiat: 82"
+print(h)
+add=(95+88+77+79+82)
+print("Total Marks \t",add)
+print("Out Of: 500")
+per=421*100/500
+print("Percentage \t",per)
+
+
